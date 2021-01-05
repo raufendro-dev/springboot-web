ca0019eeaec04af0bf8c0f4266626cf00017558a
raufendro-dev <raufendro@gmail.com>
2021-01-05 15:25:51 +0700

    First commit

---
+++ src/main/java/com/raufendro/webpribadi/WebPribadiApplication.java
@@ -0,0 +1,13 @@
+package com.raufendro.webpribadi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebPribadiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebPribadiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raufendro/webpribadi/WebPribadiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raufendro.webpribadi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebPribadiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
